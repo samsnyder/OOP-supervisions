7bbbe5735fbe517241d366396947a4103d156c4e
Sam Snyder <samsnyder96@gmail.com>
2014-11-17 15:26:11 +0000

    With question 4

--- Sup1/src/Main.java
+++ Sup1/src/Main.java
@@ -43,4 +43,36 @@ public class Main {
 		return new float[n][n];
 	}
 	
+	static int sum(int[] a){
+		int s = 0;
+		for(int i=0; i<a.length; i++)
+			s += a[i];
+		return s;
+	}
+	
+	static int[] cumsum(int[] a){
+		int[] cs = new int[a.length];
+		cs[0] = a[0];
+		for(int i=1; i<a.length; i++)
+			cs[i] = cs[i-1] + a[i];
+		return cs;
+	}
+	
+	// Very dirty! Cannot figure out a better way to do this. Normally I would use an ArrayList.
+	static int[] positives(int[] a){
+		int c=0;
+		for(int i=0; i<a.length; i++)
+			if(a[i] >= 0)
+				c++;
+		int[] p = new int[c];
+		int j = 0;
+		for(int i=0; i<a.length; i++){
+			if(a[i] >= 0){
+				p[j] = a[i];
+				j++;
+			}
+		}
+		return p;
+	}
+	
 }
